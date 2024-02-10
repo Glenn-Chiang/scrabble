dc82c403ff5fa17d69e4ef990f93b4fa14286224
Glenn Chiang <glennchiang24@gmail.com>
2024-02-10 11:49:03 +0800

    Add scrabble icon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import iconUrl from "/icon.png";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-screen h-[6vh] px-4 flex items-center bg-white z-20 shadow text-xl ">
-      <Link to={'/'} className="text-sky-500">Scrabble</Link>
+    <nav className="fixed top-0 left-0 w-screen h-[6vh] px-4 flex items-center bg-white z-20 shadow text-xl font-bold">
+      <Link to={"/"} className="text-sky-500 flex items-center gap-2">
+        <img src={iconUrl} className="w-10 h-10" />
+        Scrabble
+      </Link>
     </nav>
-  )
-}
+  );
+};
