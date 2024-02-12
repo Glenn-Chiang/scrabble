67eff18291d379707a03eeb6a5bcbdbf2455febd
Glenn Chiang <glennchiang24@gmail.com>
2024-02-12 16:13:56 +0800

    what happens if i remove the PersistGate? i am literally just shooting in the dark because the error gives me no indication whatsoever as to what could be causing the problem, or what the problem even is at all

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,9 +10,9 @@ import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <RouterProvider router={router} />
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
