7df239b7ad103257731a1deee5b890b4f0e69929
Glenn Chiang <glennchiang24@gmail.com>
2024-02-12 22:45:15 +0800

    Add api call to check word validity

--- frontend/src/lib/game-mechanics/validateWord.ts
+++ frontend/src/lib/game-mechanics/validateWord.ts
@@ -1,3 +1,7 @@
-export async function validateWord(word: string) {
-  return true;
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export async function validateWord(word: string): Promise<boolean> {
+  const response = await fetch(`${BASE_URL}/valid-words/${word}`);
+  const data: { valid: boolean } = await response.json();
+  return data.valid;
 }
