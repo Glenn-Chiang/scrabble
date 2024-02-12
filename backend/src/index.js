92e7cab4fc3c64a8baeefe77262f355df9b7b2d1
Glenn Chiang <glennchiang24@gmail.com>
2024-02-12 22:48:03 +0800

    Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const fs = require("fs").promises;
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 app.get("/valid-words/:word", async (req, res, next) => {
   try {
